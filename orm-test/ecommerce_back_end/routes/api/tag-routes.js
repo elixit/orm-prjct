a6f8dc57fbf6366e36eec709d890e9dbfe1448d4
Ismail <63372291+elixit@users.noreply.github.com>
2023-08-21 11:41:59 -0700

    Update tag-routes.js

--- orm-test/ecommerce_back_end/routes/api/tag-routes.js
+++ orm-test/ecommerce_back_end/routes/api/tag-routes.js
@@ -5,7 +5,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', async (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+
   try {
     const findTag = await Tag.findAll({
       include: {
@@ -27,7 +27,7 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  
   try {
     const findTag = await Tag.findByPk(req.params.id, {
       include: {
