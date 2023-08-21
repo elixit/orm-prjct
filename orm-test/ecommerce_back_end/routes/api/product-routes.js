eb1a52abb52dc65ee8d672ee98bd165f06bb9e75
Ismail <63372291+elixit@users.noreply.github.com>
2023-08-21 11:45:38 -0700

    Update product-routes.js

--- orm-test/ecommerce_back_end/routes/api/product-routes.js
+++ orm-test/ecommerce_back_end/routes/api/product-routes.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
 
-// get all products
+
 router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+
   try {
     const findProduct = await Product.showAll({
       attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
@@ -33,10 +32,9 @@ router.get('/', async (req, res) => {
   }
 });
 
-// get one product
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+
   try {
     const findProduct = await Product.findByPk(req.params.id,{
       attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
